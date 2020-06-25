82ac3d430f9acefce00d8b627c4c025ffad6d717
Sergiu Popescu <sergiupopescu2@gmail.com>
2020-06-25 23:16:44 +0200

    deleted spaghetti code

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,38 +1,26 @@
-registerShortcut("MoveWindowToCenter", "Move Window to Center", "Meta+C", function () {
+
+function moveWindow(x, y) {
     var client = workspace.activeClient;
     if (client.moveable) {
         var maxArea = workspace.clientArea(KWin.MaximizeArea, client);
         client.geometry = {
-            x: maxArea.x + (maxArea.width - client.width) / 2,
-            y: maxArea.y + (maxArea.height - client.height) / 2,
+            x: maxArea.x + (maxArea.width - client.width) / x,
+            y: maxArea.y + (maxArea.height - client.height) / y,
             width: client.width,
             height: client.height
         };
     }
+}
+
+
+registerShortcut("MoveWindowToCenter", "Move Window to Center", "Meta+C", function () {
+    moveWindow(2, 2);
 });
 
 registerShortcut("MoveWindowToLeft", "Move Window to Left", "Meta+X", function () {
-    var client = workspace.activeClient;
-    if (client.moveable) {
-        var maxArea = workspace.clientArea(KWin.MaximizeArea, client);
-        client.geometry = {
-            x: maxArea.x + (maxArea.width - client.width) / 10000,
-            y: maxArea.y + (maxArea.height - client.height) / 2,
-            width: client.width,
-            height: client.height
-        };
-    }
+    moveWindow(10000, 2);
 });
 
 registerShortcut("MoveWindowToRight", "Move Window to Right", "Meta+V", function () {
-    var client = workspace.activeClient;
-    if (client.moveable) {
-        var maxArea = workspace.clientArea(KWin.MaximizeArea, client);
-        client.geometry = {
-            x: maxArea.x + (maxArea.width - client.width) / 1,
-            y: maxArea.y + (maxArea.height - client.height) / 2,
-            width: client.width,
-            height: client.height
-        };
-    }
+    moveWindow(1, 2);
 });
